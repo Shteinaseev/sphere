8b447c41eaec6467a62595c82fa7e2e2dbbd37ad
Shteinaseev <142624493+Shteinaseev@users.noreply.github.com>
2025-06-11 16:02:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ for(i = 0; i < 180; i += 9){
 }
 
 sphere.innerHTML = rows.join("");
+
+const headerEl = document.querySelector(".header")
+
+window.addEventListener("scroll", function(){
+    const scrollPos = window.scrollY
+
+    if(scrollPos > 100) {
+        headerEl.classList.add("header__scroll")
+    }else{
+        headerEl.classList.remove("header__scroll")
+    }
+})
+
+const menuIcon = document.querySelector('#menu-icon')
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-i')
+} )
+
